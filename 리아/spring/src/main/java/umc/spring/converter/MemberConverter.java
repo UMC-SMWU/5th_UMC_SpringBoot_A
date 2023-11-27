76111d738f056df46e7dd889951f7fe1435b33a6
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2023-11-27 16:33:07 +0900

    Feat: converter 생성

---
+++ 리아/spring/src/main/java/umc/spring/converter/MemberConverter.java
@@ -0,0 +1,18 @@
+package umc.spring.converter;
+
+import umc.spring.domain.Member;
+import umc.spring.domain.enums.Gender;
+import umc.spring.web.dto.MemberRequestDTO;
+import umc.spring.web.dto.MemberResponseDTO;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class MemberConverter {
+    public static MemberResponseDTO.JoinResultDTO toJoinResultDTO(Member member){
+        return MemberResponseDTO.JoinResultDTO.builder()
+                .memberId(member.getId())
+                .createdAt(LocalDateTime.now())
+                .build();
+    }
+}
