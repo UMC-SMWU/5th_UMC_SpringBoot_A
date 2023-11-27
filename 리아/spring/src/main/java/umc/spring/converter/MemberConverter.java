50294f8c3ffd24cd9febd769baa3e762c5776dbb
skfl0282 <90668082+skfl0282@users.noreply.github.com>
2023-11-27 16:35:30 +0900

    Feat: Converter에서 멤버 생성

--- 리아/spring/src/main/java/umc/spring/converter/MemberConverter.java
+++ 리아/spring/src/main/java/umc/spring/converter/MemberConverter.java
@@ -15,4 +15,28 @@ public class MemberConverter {
                 .createdAt(LocalDateTime.now())
                 .build();
     }
+
+    public static Member toMember(MemberRequestDTO.JoinDto request){
+        Gender gender = null;
+
+        switch (request.getGender()){
+            case 1:
+                gender = Gender.MALE;
+                break;
+            case 2:
+                gender = Gender.FEMALE;
+                break;
+            case 3:
+                gender = Gender.NONE;
+                break;
+        }
+
+        return Member.builder()
+                .address(request.getAddress())
+                .specAddress(request.getSpecAddress())
+                .gender(gender)
+                .name(request.getName())
+                .memberPreferList(new ArrayList<>())
+                .build();
+    }
 }
